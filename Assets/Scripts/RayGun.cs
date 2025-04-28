d63b233693bb05099b5a213060ee72bd0eef81c3
spdvi <gjm@paucasesnovescifp.cat>
2025-04-28 14:03:08 +0200

    Raycast and ray impact.

--- Assets/Scripts/RayGun.cs
+++ Assets/Scripts/RayGun.cs
@@ -7,17 +7,36 @@ public class RayGun : MonoBehaviour
     public LineRenderer linePrefab;
     public Transform shootingPoint;
     public float maxLineDistance = 5;
-    public float lineShowTimer = 0.1f;
+    public float lineShowTimer = 0.05f;
+    public LayerMask layerMask;
+    public GameObject rayImpactPrefab;
 
     void Update()
     {
         if (OVRInput.GetDown(shootingButton))
         {
             //Debug.Log("Shoot");
+            Ray ray = new Ray(shootingPoint.position, shootingPoint.forward);
+            bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layerMask);
+            Vector3 endPoint = Vector3.zero;
+            
+            if (hasHit)
+            {
+                //stop the ray
+                endPoint = hit.point;
+                Quaternion rayImpactRotation = Quaternion.LookRotation(-hit.normal);
+                GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point, rayImpactRotation);
+                Destroy(rayImpact, 1f);
+            }
+            else
+            {
+                endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+            }
+            
             LineRenderer line = Instantiate(linePrefab);
             line.positionCount = 2;
             line.SetPosition(0, shootingPoint.position);
-            Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+
             line.SetPosition(1, endPoint);
             Destroy(line.gameObject, lineShowTimer);
         }
